bfa2a591b6011b6480ca3d8a5f78d2f7a067dcbe
harmankumar <harmankumar1995@gmail.com>
2016-10-03 12:28:22 +0530

    Adding Stuff Done Till Now

--- test/detect_aruco_image_test.py
+++ test/detect_aruco_image_test.py
@@ -35,12 +35,12 @@ if __name__ == '__main__':
     marker_size = 0.165
 
     # Load an color image in grayscale
-    img = cv2.imread('6.png', cv2.IMREAD_COLOR)
+    img = cv2.imread('5.png', cv2.IMREAD_COLOR)
 
     markers = markerdetector.detect(img, camparam)
     print "Markers detected", len(markers)
     for marker in markers:
-        # marker.draw(img, np.array([255, 0, 0]), 100, True)
+        marker.draw(img, np.array([255, 0, 0]), 10, True)
         # for i, point in enumerate(marker):
         #     print i, point
 
