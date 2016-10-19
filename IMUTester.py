ef77806f898bbd33100d7d9c5dff5f766dbde829
harmankumar <harmankumar1995@gmail.com>
2016-10-19 13:25:50 +0530

    Adding Scripts to get IMU Data

---
+++ IMUTester.py
@@ -0,0 +1,16 @@
+import socket, traceback
+# Note that the IP Address and Port in this script and the script on the Mobile Phone should match.
+host = "10.192.50.36"   #IP Address of this Machine
+port = 5555
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((host, port))
+while 1:
+    try:
+        message, address = s.recvfrom(8192)
+        print message
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        traceback.print_exc()
