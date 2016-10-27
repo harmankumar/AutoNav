f7fc06f605c58f677f2c986ca3fa1dfa082f18b0
harmankumar <harmankumar1995@gmail.com>
2016-10-27 10:50:00 +0530

    Getting IMU Data Now

--- IMUTester.py
+++ IMUTester.py
@@ -1,14 +1,14 @@
 import socket, traceback
 # Note that the IP Address and Port in this script and the script on the Mobile Phone should match.
 host = "10.192.50.36"   #IP Address of this Machine
-port = 5555
+port = 3400
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind((host, port))
 while 1:
     try:
-        message, address = s.recvfrom(8192)
+        message, address = s.recvfrom(4096)
         print message
     except (KeyboardInterrupt, SystemExit):
         raise
